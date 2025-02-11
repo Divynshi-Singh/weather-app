af65862dd935a488aa604d48e8a51605d1011ead
Divynshi-Singh <divya.singh@innotechteam.in>
2025-02-11 17:28:19 +0530

    implement in js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const searchInput = document.querySelector('.search-input');
 const searchButton = document.querySelector('.search-button');
 const weatherInfo = document.querySelector('.weather-info');
@@ -9,6 +8,8 @@ const descriptionElement = document.querySelector('.description');
 const locationElement = document.querySelector('.location');
 const loadingSpinner = document.querySelector('.loading-spinner');
 const apiKey = '82005d27a116c2880c8f0fcb866998a0';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+
 const weatherIcons = {
     '01d': '01d.png',
     '02d': '02d.png',
@@ -32,7 +33,6 @@ const weatherIcons = {
 
 
 
-const BASE_URL = `https://api.openweathermap.org/data/2.5`;
 
 function weatherInformation() {
     const weatherDetailsContainer = document.querySelector('.weather-details');
@@ -81,7 +81,6 @@ function weatherInformation() {
         valueContainers[detail.label.toLowerCase().replace(' ', '')] = valueContainer;
     });
 
-
     function setupForecastModal() {
         const forecastButton = document.createElement('button');
         forecastButton.textContent = 'Forecast';
@@ -128,6 +127,7 @@ function weatherInformation() {
             });
         }
     }
+
     setupForecastModal();
     return valueContainers;
 }
@@ -169,8 +169,8 @@ function displayWeatherData(data) {
         console.error('Incomplete weather data:', data);
         errorMessage.textContent = 'Weather data is missing or incomplete.';
         errorMessage.style.display = 'block';
-      loadingSpinner.style.display = 'none';
-  }
+        loadingSpinner.style.display = 'none';
+    }
 }
 
 searchButton.addEventListener('click', function () {
@@ -179,22 +179,21 @@ searchButton.addEventListener('click', function () {
     errorMessage.style.display = 'none';
     loadingSpinner.style.display = 'block';
     if (city.trim() === '') {
-        errorMessage.textContent = ' Enter a city name.';
+        errorMessage.textContent = 'Enter a city name.';
         errorMessage.style.display = 'block';
         loadingSpinner.style.display = 'none';
         return;
     }
     loadingSpinner.style.display = 'block';
     fetchData('weather', city);
-    // Clear the search input field after the search button is clicked
     searchInput.value = '';  
 });
+
 function fetchData(endPoint, city) {
     fetch(`${BASE_URL}/${endPoint}?q=${city}&appid=${apiKey}&units=metric`)
-
         .then(response => {
             if (!response.ok) {
-                throw new Error('City not found');
+                throw new Error(' not found');
             }
             return response.json();
         })
@@ -203,16 +202,16 @@ function fetchData(endPoint, city) {
             console.log(data);
             loadingSpinner.style.display = 'none';
         })
-
         .catch((error) => {
             console.error('Error fetching weather data:', error);
-            errorMessage.textContent = 'city not found.';
+            errorMessage.textContent = 'City not found.';
             errorMessage.style.display = 'block';
             loadingSpinner.style.display = 'none';
-       });
+        });
 }
+
 function fetchHourlyForecast(city) {
-    const url = `${BASE_URL}/forecast?q=${city}&appid=${apiKey}&units=metric`
+    const url = `${BASE_URL}/forecast?q=${city}&appid=${apiKey}&units=metric`;
     fetch(url)
         .then(response => {
             if (!response.ok) {
@@ -229,8 +228,9 @@ function fetchHourlyForecast(city) {
             errorMessage.style.display = 'block';
         });
 }
+
 function fetchHourlyForecastByCoordinates(lat, lon) {
-    const url = `${BASE_URL}/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+    const url = `${BASE_URL}/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
     fetch(url)
         .then(response => {
             if (!response.ok) {
@@ -245,24 +245,25 @@ function fetchHourlyForecastByCoordinates(lat, lon) {
             console.error('Error fetching forecast data by coordinates:', error);
             errorMessage.textContent = 'Unable to fetch forecast data. Please try again later.';
             errorMessage.style.display = 'block';
-                });
+        });
 }
+
 function displayHourlyForecast(data) {
     const forecastModal = document.getElementById('forecastModal');
     const modalBackdrop = document.getElementById('modalBackdrop');
     const chartCanvas = document.getElementById('hourlyForecastChart');
     const tableBody = document.getElementById('forecastTableBody');
 
-    tableBody.innerHTML = ''; // Clear existing forecast table
-     // Check if the canvas element exists
+    tableBody.innerHTML = ''; 
+
     if (!chartCanvas) {
         console.error("Chart canvas element not found.");
         return;
     }
-    const ctx = chartCanvas.getContext('2d');
-    ctx.clearRect(0, 0, chartCanvas.width, chartCanvas.height);  // Clear the canvas
 
-    // Get the hourly data (next 8 hours)
+    const ctx = chartCanvas.getContext('2d');
+    ctx.clearRect(0, 0, chartCanvas.width, chartCanvas.height); 
+
     const hourlyData = data.list.slice(0, 8);
     const hourlyWindSpeeds = hourlyData.map(item => Math.round(item.wind.speed));
     const hourlyLabels = hourlyData.map(item => {
@@ -343,13 +344,13 @@ function displayHourlyForecast(data) {
         const iconFilename = weatherIcons[weatherIconCode] || 'default.png';  
 
         const row = document.createElement('tr');
-        row.innerHTML = `
-            <td>${dayString}</td>
+        row.innerHTML = 
+            `<td>${dayString}</td>
             <td class="forecast-icon"><img src="${iconFilename}" alt="Weather icon">${Math.round(item.main.temp)}°C</td>
-            <td>${item.weather[0].description}</td>
-        `;
+            <td>${item.weather[0].description}</td>`;
         tableBody.appendChild(row);
     });
+
     if (forecastModal && modalBackdrop) {
         forecastModal.style.display = 'block';
         modalBackdrop.style.display = 'block';
@@ -362,11 +363,13 @@ function displayHourlyForecast(data) {
         modalBackdrop.style.display = 'none';
     });
 }
+
 searchInput.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
         searchButton.click();
     }
-})
+});
+
 function fetchWeatherByCoordinates(lat, lon) {
     fetch(`${BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
         .then(response => {
@@ -388,7 +391,7 @@ function fetchWeatherByCoordinates(lat, lon) {
         })
         .catch(error => {
             console.error('Error fetching weather data by coordinates:', error);
-            errorMessage.textContent = 'Unable to fetch weather data. Please try again later.';
+            errorMessage.textContent = 'Unable to fetch weather data Please try again later.';
             errorMessage.style.display = 'block';
             loadingSpinner.style.display = 'none';
         });
@@ -406,14 +409,15 @@ function getCoordinates() {
                 },
                 error => {
                     console.error('Error getting geolocation:', error);
-                    reject(new Error('Unable to retrieve your location. Please try again or enter a city name.'));
+                    reject(new Error('Unable to retrieve your location. Please try again'));
                 }
             );
         } else {
-            reject(new Error('Geolocation is not supported by this browser.'));
+            reject(new Error('Geolocation is not support by this browser.'));
         }
     });
 }
+
 function getCurrentLocationWeather() {
     loadingSpinner.style.display = 'block';
     getCoordinates()
@@ -429,6 +433,7 @@ function getCurrentLocationWeather() {
             loadingSpinner.style.display = 'none';
         });
 }
+
 window.onload = function () {
     getCurrentLocationWeather();
 };
